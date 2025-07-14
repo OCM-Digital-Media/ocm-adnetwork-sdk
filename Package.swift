e43046f33cf14bcd6e9ecd1ab23ae3488b30fdf1
Predrag Jevtic <predragjevtic@gmail.com>
2025-07-14 13:39:12 +0200

    ver_1.0.6

--- Package.swift
+++ Package.swift
@@ -13,22 +13,22 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "OCMAdNetworkIOS",
-            url: "https://cdn.orangeclickmedia.com/sdk/1.0.6/OCMAdNetworkIOS.xcframework.zip",
+            url: "https://storage.googleapis.com/cdn.orangeclickmedia.com/sdk/1.0.6/OCMAdNetworkIOS.xcframework.zip",
             checksum: "7312515ffdb1b3837e43d2f0f6b5c39c49cc37544719b0e2e354da1ed8dd719e"
         ),
         .binaryTarget(
             name: "PrebidMobile",
-            url: "https://cdn.orangeclickmedia.com/sdk/1.0.6/PrebidMobile.xcframework.zip",
+            url: "https://storage.googleapis.com/cdn.orangeclickmedia.com/sdk/1.0.6/PrebidMobile.xcframework.zip",
             checksum: "ad4b7263351ea3c34b97d35152d9d33e72b319fccda247cf93d4d63c4c4931ba"
         ),
         .binaryTarget(
             name: "GoogleMobileAds",
-            url: "https://cdn.orangeclickmedia.com/sdk/1.0.6/GoogleMobileAds.xcframework.zip",
+            url: "https://storage.googleapis.com/cdn.orangeclickmedia.com/sdk/1.0.6/GoogleMobileAds.xcframework.zip",
             checksum: "de3580f7a70d68618ba342132155fd6ef270ea26a055ad17ff511ecf114a9622"
         ),
         .binaryTarget(
             name: "UserMessagingPlatform",
-            url: "https://cdn.orangeclickmedia.com/sdk/1.0.6/UserMessagingPlatform.xcframework.zip",
+            url: "https://storage.googleapis.com/cdn.orangeclickmedia.com/sdk/1.0.6/UserMessagingPlatform.xcframework.zip",
             checksum: "b94c0188bb2af6fa8b4cf7933728d5c6b4ba117d697632f3d0ce0d0ab14c4b93"
         ),
         .target(
@@ -38,6 +38,7 @@ let package = Package(
                 "PrebidMobile",
                 "GoogleMobileAds",
                 "UserMessagingPlatform"
+                
             ],
             path: "Sources/ocm-ad-network-sdk-wrapper"
         )
